d5c43a345205d637aad11c051350c7731a4e643c
shivamvr <shivamweb2@gmail.com>
2023-06-14 22:56:33 +0530

    Routes Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import AllRoutes from "./routes/AllRoutes";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello this is Shivam Verma</h1>
+      <AllRoutes />
+    </div>
+  );
+}
